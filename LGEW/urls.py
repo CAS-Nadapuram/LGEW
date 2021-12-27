33fbe2b7612d9ab203c3fcd8a2375de568674bc4
Vaishnav <vrv.mee@gmail.com>
2021-12-28 03:14:38 +0530

    some updates in settings

--- LGEW/urls.py
+++ LGEW/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("maincode.urls"))
+    path('',include("MainCode.urls"))
 ]
--- MainCode/apps.py
+++ MainCode/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MaincodeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'maincode'
+    name = 'MainCode'
