67749f23a0ff7daf85e40b0e3148019566eeda5c
Vaishnav <vrv.mee@gmail.com>
2022-04-23 18:46:33 +0530

    added allowed host

--- LGEW/settings.py
+++ LGEW/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l7#tc$+=-glnqg-h==o1!^4d$eh4vtqr6gjj2^6tk)z$!v202j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","192.168.1.10"]
+ALLOWED_HOSTS = ["127.0.0.1","192.168.1.10","https://projectcas.herokuapp.com/"]
 
 
 # Application definition
@@ -141,8 +141,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
-# STATIC_ROOT=os.path.join(BASE_DIR,'assets')
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
